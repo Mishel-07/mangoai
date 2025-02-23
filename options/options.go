e143eeed2e6e89698cbde0cea4382921647bff22
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-02-23 20:16:43 +0530

    Update options.go

--- options/options.go
+++ options/options.go
@@ -4,6 +4,10 @@ import "github.com/Mishel-07/mangoai"
 
 type Option func(*mangoai.Mango)
 
+func RoleUser() Option{
+        return "user"
+}
+
 func WithBaseURL(url string) Option {
         return func(m *mangoai.Mango) {
             m.baseURL = url
