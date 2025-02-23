0605b76a633f3a03ab694a871cb75b51b51cfdad
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-02-23 20:48:43 +0530

    Update options.go

--- options/options.go
+++ options/options.go
@@ -1,20 +1,7 @@
 package options
 
-type Mango struct {
-        baseURL string
-        Chat    interface{}
-}
-
-type Option func(*Mango)
-
 const RoleUser = "user"
 
 const RoleSystem = "system"
 
-const RoleAssistant = "assistant"
-
-func WithBaseURL(url string) Option {
-        return func(m *Mango) {
-            m.baseURL = url
-        }
-}
+const RoleAssistant = "assistant
