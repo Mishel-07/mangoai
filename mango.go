fd879802432c41458f43c1d0f361096b5fa1f9f0
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-02-23 18:21:48 +0530

    Update mango.go

--- mango.go
+++ mango.go
@@ -1,27 +1,27 @@
 package mangoai
 
 type Mango struct {
-    baseURL string
-    Chat    *Chat
+        baseURL string
+        Chat    *Chat
 }
 
 type Option func(*Mango)
 
 func WithBaseURL(url string) Option {
-    return func(m *Mango) {
-        m.baseURL = url
-    }
+        return func(m *Mango) {
+                m.baseURL = url
+        }
 }
 
 func NewMango(options ...Option) *Mango {
-    mango := &Mango{
-        baseURL: "https://mangooapi.onrender.com",
-    }
+        mango := &Mango{
+                baseURL: "https://mangooapi.onrender.com",
+        }
 
-    for _, option := range options {
-        option(mango)
-    }
+        for _, option := range options {
+                option(mango)
+        }
 
-    mango.Chat = NewChat(mango) 
-    return mango
+        mango.Chat = NewChat(mango) 
+        return mango
 }
