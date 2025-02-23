b5e8eaf571720a989f122c99fbfdd4dd2b4911e9
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-02-23 16:47:20 +0530

    Create client.go

---
+++ client.go
@@ -0,0 +1,27 @@
+package mango
+
+type Mango struct {
+	baseURL string
+	Chat    *Chat
+}
+
+type Option func(*Mango)
+
+func WithBaseURL(url string) Option {
+	return func(m *Mango) {
+		m.baseURL = url
+	}
+}
+
+func NewMango(options ...Option) *Mango {	
+	mango := &Mango{
+		baseURL: "https://mangooapi.onrender.com",
+	}
+	
+	for _, option := range options {
+		option(mango)
+}
+	
+	mango.Chat = &Chat{mango: mango}
+	return mango
+}
